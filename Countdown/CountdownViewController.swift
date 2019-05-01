1e8ee0e5462dcd3854468486200a49629aee339a
Jeffrey Carpenter <spaz926@me.com>
2019-05-01 11:41:02 -0500

    Hooked up UI Elements to Code

--- Countdown/CountdownViewController.swift
+++ Countdown/CountdownViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class CountdownViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+    }
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
     }
 }
